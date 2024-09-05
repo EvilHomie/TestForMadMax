3efd43c47bc098b0b48422ef253a41f3a084508d
EvilHomie <132207110+EvilHomie@users.noreply.github.com>
2024-09-05 18:00:37 +0300

    Replace Font For support Ru, Create UI LevelStatistic

--- Assets/Scripts/AditionClasses/TextConstants.cs
+++ Assets/Scripts/AditionClasses/TextConstants.cs
@@ -123,6 +123,7 @@ public static class TextConstants
             HEADLIGHTS = "Фар";
             EXPLOSIVEPARTS = "Взрывчатых частей";
             TIP = "Совет";
+            NEXTTIP = "Далее";
 
         }
 
